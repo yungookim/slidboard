1b877f9263b945ddf33e13aee33783a0a0bee5c5
KimY <yungookim@gmail.com>
2012-10-09 15:03:21 -0400

    winning condition, new board spawn

--- Practice/tictactoe/tictactoe/SurfaceWindow1.xaml.cs
+++ Practice/tictactoe/tictactoe/SurfaceWindow1.xaml.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Windows;
@@ -18,16 +18,157 @@ using Microsoft.Surface.Presentation.Input;
 
 namespace tictactoe
 {
-    
+
+    public class TTTboard
+    {
+
+        ScatterViewItem viewItem;
+        ScatterView main_view;
+
+        string current = "O";
+        string[,] perGrid = new String[3, 3];
+        bool win = false;
+
+        public TTTboard(ScatterView mv) 
+        {
+            this.viewItem = createNewBoard();
+            this.main_view = mv;
+            //Init grid
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    perGrid[i, j] = "";
+                }
+            }
+
+        }
+
+        public string[,] getGrid()
+        {
+            return this.perGrid;
+        }
+
+        public ScatterViewItem getView()
+        {
+            return this.viewItem;
+        }
+
+
+        private void change(object sender, RoutedEventArgs e)
+        {
+            SurfaceButton cur_ele = (SurfaceButton)sender;
+            string content = cur_ele.Content.ToString();
+
+            if (content.Equals("") && (!win)) // if the cell is empty, and havn't found a winner yet.
+            {
+                int col = Grid.GetColumn(cur_ele);
+                int row = Grid.GetRow(cur_ele);
+
+                cur_ele.Content = current;
+
+                current = current.Equals("X") ? "O" : "X";
+
+                perGrid[row, col] = current; // set the cell content as marked sign
+
+                win = winner_Checker(current); // check winner
+                if (win)
+                {
+                    TTTboard _new = new TTTboard(main_view);
+                    main_view.Items.Add(_new.getView());
+                }
+            }
+            else if (content.Equals("") && (win)) // if the cell is empty, and found a winner.
+            {
+                cur_ele.IsEnabled = false;// disable the cell from marking new one.
+
+                return;
+            }
+        }
+
+        private bool winner_Checker(string content)
+        {
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (perGrid[i, 0].Equals(content) && perGrid[i, 1].Equals(content) && perGrid[i, 2].Equals(content))
+                {
+                    return true;
+                }
+                else if (perGrid[0, i].Equals(content) && perGrid[1, i].Equals(content) && perGrid[2, i].Equals(content))
+                {
+                    return true;
+                }
+            }
+
+            if (perGrid[0, 0].Equals(content) && perGrid[1, 1].Equals(content) && perGrid[2, 2].Equals(content))
+            {
+                return true;
+            }
+            else if (perGrid[0, 2].Equals(content) && perGrid[1, 1].Equals(content) && perGrid[2,0].Equals(content))
+            {
+                return true;
+            }
+            
+            return false;
+        }
+
+
+        //Factory function : Add a new game board to the main scatter view
+        private ScatterViewItem createNewBoard()
+        {
+            ScatterViewItem sci = new ScatterViewItem();
+            Grid grid = new Grid();
+
+            //Set definitions
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.ColumnDefinitions.Add(new ColumnDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+            grid.RowDefinitions.Add(new RowDefinition());
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    grid.Children.Add(createSB(i, j));
+                }
+            }
+
+            sci.Height = 300;
+            sci.Content = grid;
+            return sci;
+        }
+
+
+
+        //Factory Function : Create a new SurfaceButton
+        private SurfaceButton createSB(int row, int col)
+        {
+            SurfaceButton sb = new SurfaceButton();
+            sb.Margin = new Thickness(5);
+            sb.Height = 100;
+            sb.Width = 100;
+            sb.HorizontalContentAlignment = HorizontalAlignment.Center;
+            sb.VerticalContentAlignment = VerticalAlignment.Center;
+            sb.Content = "";
+
+            //Set row and column
+            Grid.SetColumn(sb, row);
+            Grid.SetRow(sb, col);
+
+            sb.Click += new RoutedEventHandler(change);
+            return sb;
+        }
+    }
+
 
     /// <summary>
     /// Interaction logic for SurfaceWindow1.xaml
     /// </summary>
     public partial class SurfaceWindow1 : SurfaceWindow
     {
-
-        string[,] perGrid = new string[3,3];
-        string current = "O";
 
         /// <summary>
         /// Default constructor.
@@ -39,16 +180,8 @@ namespace tictactoe
             // Add handlers for window availability events
             AddWindowAvailabilityHandlers();
 
-            //Init perGrid
-            for (int i = 0; i < 3; i++){
-                for (int j = 0; j < 3; j++){
-                    perGrid[i, j] = "";
-                }
-            }
-
-            board_view.Items.Add(createNewBoard());
-            board_view.Items.Add(createNewBoard());
-            board_view.Items.Add(createNewBoard());
+            TTTboard _new = new TTTboard(board_view);
+            board_view.Items.Add(_new.getView());
         }
 
         /// <summary>
@@ -117,149 +250,6 @@ namespace tictactoe
             //TODO: disable audio, animations here
         }
 
-        private void btn_Click(object sender, RoutedEventArgs e)
-        {
-            SurfaceButton btn = (SurfaceButton)sender;
 
-            if (btn.Content.ToString().Length > 0)
-            {
-                return;
-            }
-            /*
-            if (current == 0)
-            {
-                btn.Content = "X";
-                current = 1;
-                int row = int.Parse(btn.Name.Substring(3, 1));
-                int col = int.Parse(btn.Name.Substring(4, 1));
-                perGrid[row, col] = "X";
-            }
-            else
-            {
-                btn.Content = "O";
-                current = 0;
-                int row = int.Parse(btn.Name.Substring(3, 1));
-                int col = int.Parse(btn.Name.Substring(4, 1));
-                perGrid[row, col] = "O";
-            }
-
-            // if win, then disable all other button left over.
-            if (winner_Checker(btn.Content.ToString()))
-            {
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        perGrid[i, j] = "";
-                    }
-                }
-                //duplicateGrid();
-            }
-             */
-        }
-
-        private bool winner_Checker(string content) {
-
-            for (int i = 0; i < 3; i++)
-            {
-                if (perGrid[i, 0].Equals(perGrid[i, 1].Equals(perGrid[i, 2].Equals(content))))
-                {
-                    return true;
-                }
-                else if (perGrid[0, i].Equals(content) && perGrid[1, i].Equals(content) && perGrid[2, i].Equals(content))
-                {
-                    return true;
-                }
-            }
-
-            if (perGrid[0, 0].Equals(content) && perGrid[1, 1].Equals(content) && perGrid[2, 2].Equals(content))
-            {
-                return true;
-            }
-            else if (perGrid[0, 2].Equals(content) && perGrid[1, 1].Equals(content) && perGrid[2,0].Equals(content))
-            {
-                return true;
-            }
-
-            return false;
-        }
-
-        //Factory function : Add a new game board to the main scatter view
-        private ScatterViewItem createNewBoard()
-        {
-
-           
-
-            ScatterViewItem sci = new ScatterViewItem();
-            Grid grid = new Grid();
-
-            /*
-            GridLength GL = new GridLength(100, GridUnitType.Pixel);
-            ColumnDefinition c = new ColumnDefinition();
-            c.Width = GL;
-            ColumnDefinition c1 = new ColumnDefinition();
-            ColumnDefinition c2 = new ColumnDefinition();
-
-            RowDefinition r = new RowDefinition();
-            r.Height = GL;
-            RowDefinition r1 = new RowDefinition();
-            RowDefinition r2 = new RowDefinition();
-            */
-
-            //Set definitions
-            grid.ColumnDefinitions.Add(new ColumnDefinition());
-            grid.ColumnDefinitions.Add(new ColumnDefinition());
-            grid.ColumnDefinitions.Add(new ColumnDefinition());
-            grid.RowDefinitions.Add(new RowDefinition());
-            grid.RowDefinitions.Add(new RowDefinition());
-            grid.RowDefinitions.Add(new RowDefinition());
-
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    grid.Children.Add(createSB(i, j));
-                }
-            }
-
-            sci.Height = 300;
-            sci.Content = grid;
-            return sci;
-        }
-
-        //Factory Function : Create a new SurfaceButton
-        private SurfaceButton createSB(int row, int col)
-        {
-            SurfaceButton sb = new SurfaceButton();
-            sb.Margin = new Thickness(5);
-            sb.Height = 100;
-            sb.Width = 100;
-            sb.HorizontalContentAlignment = HorizontalAlignment.Center;
-            sb.VerticalContentAlignment = VerticalAlignment.Center;
-            sb.Content = "";
-
-            //Set row and column
-            Grid.SetColumn(sb, row);
-            Grid.SetRow(sb, col);
-
-            sb.Click += new RoutedEventHandler(change);
-            return sb;
-        }
-
-        private void change(object sender, RoutedEventArgs e)
-        {
-            SurfaceButton cur_ele = (SurfaceButton)sender;
-            string content = cur_ele.Content.ToString();
-
-            if (content.Equals(""))
-            {
-
-
-                Console.WriteLine(Grid.GetColumn(cur_ele) + "x" + Grid.GetRow(cur_ele));
-
-
-                cur_ele.Content = current;
-                current = current.Equals("X") ? "O" : "X";
-            } 
-        }
     }
 }
