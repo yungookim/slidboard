f6f1b7b6d0395d7677d56c78b02498bff86be92d
kimy simplyi <yungookim@gmail.com>
2012-10-24 20:28:34 -0400

    server improvement

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
 var net = require('net');
 
 var server = net.createServer(function (socket){
+	socket.setEncoding('utf-8');	
 	console.log('server running');
+	socket.on('data', function(data){
+		console.log(data);
+		socket.write("Accepted ");
+		socket.pipe(socket);
+		socket.end();
+	});
 	socket.on('end', function(){
 		console.log('server disconnected');
 	});
-	socket.write('hello\n');
-	socket.pipe(socket);
 });
 
 var PORT = 6060;
