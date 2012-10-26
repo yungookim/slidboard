a26d17dcbc26d311e883bd132d73c7c394bbaad2
kimy simplyi <yungookim@gmail.com>
2012-10-26 17:58:34 -0400

    asdf

--- server/app.js
+++ server/app.js
@@ -2,13 +2,31 @@ var net = require('net');
 var PORT = 6060;
 
 var server = net.createServer(function (socket){
-	socket.setEncoding('utf-8');	
-	console.log('Accepting Connection');
+	socket.setEncoding('utf-8');
+
+	var pixelSense = socket.remoteAddress;
+
+	console.log('Accepting Connection from ' + socket.remoteAddress);
 	socket.on('data', function(data){
-		console.log(data);
+		console.log(JSON.parse(data));
+
+
 		socket.write("Accepted ");
 		socket.pipe(socket);
+
 		socket.end();
+		
+		setTimeout(function(){	
+				var newSocket = new net.Socket();
+				console.log('starting');
+				newSocket.connect(6060, pixelSense, function(_socket){
+					console.log("writing to socket");
+					_socket.write("Hello PixelSense!\n");
+					_socket.pipe(_socket);
+				});
+		}, 1000);
+
+
 	});
 	socket.on('end', function(){
 		console.log('server disconnected');
@@ -18,3 +36,4 @@ var server = net.createServer(function (socket){
 server.listen(PORT, function(){
 	console.log('Server Listening on '+ PORT);
 });
+
