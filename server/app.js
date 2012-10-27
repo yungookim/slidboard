f49163a4c6c7e37b90a1ef0e0bf48283191da512
kimy simplyi <yungookim@gmail.com>
2012-10-27 17:24:27 -0400

    server improvements

--- server/app.js
+++ server/app.js
@@ -1,20 +1,31 @@
 var net = require('net');
-var PORT = 6060;
+var PORT = 6060,
+    CLIENT_MOBILE = "MOBILE",
+    CLIENT_PIXELSENSE = "PS",
+    pixelSense,
+    mobiles = [];
+
+
 
 var server = net.createServer(function (socket){
 	socket.setEncoding('utf-8');
-
-	var pixelSense = socket.remoteAddress;
-
 	console.log('Accepting Connection from ' + socket.remoteAddress);
+
 	socket.on('data', function(data){
+		data = JSON.parse(data);
 		//console.log(JSON.parse(data));
-		console.log(data);
-		socket.write("Accepted ");
-		socket.pipe(socket);
-
+		if (data.type === CLIENT_MOBILE){
+			var item = {
+				socket : socket,
+				uuid : data.uuid
+			};
+			mobiles.push(item);
+		} else if (data.type === CLIENT_PIXELSENSE){
+			pixelSense = socket;
+		}
+		socket.write('Hello!\n');
+		console.log(mobiles);
 		socket.end();
-
 	});
 	socket.on('end', function(){
 		console.log('server disconnected');
