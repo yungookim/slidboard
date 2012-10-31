22486c102ab63a7406fe521ccd4cccade8c3ff23
kimy simplyi <yungookim@gmail.com>
2012-10-30 21:38:58 -0400

    server updates

--- server/app.js
+++ server/app.js
@@ -4,17 +4,29 @@ var net = require('net'),
 
 var server = net.createServer(function (socket){
 	socket.setEncoding('utf-8');
+	socket.setNoDelay(true);
+	socket.setKeepAlive(true);
 	console.log('Accepting Connection from ' + socket.remoteAddress);
 
+	socket.write("hello");
+	socket.write("hello2");
+	socket.end();
+	
 	socket.on('data', function(data){
-		APICalls.exec(data, socket);
+		//APICalls.exec(data, socket);
+		console.log(data);		
 	});
 	socket.on('end', function(){
 		console.log('server disconnected');
 		socket.end();
+		socket.destroy();
 	});
 	socket.on('timeout', function(){
 		socket.end();
+		socket.destroy();
+	});
+	socket.on('error', function(e){
+		console.log(e);
 	});
 });
 
