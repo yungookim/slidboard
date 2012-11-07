b4d7ce59cdc7da25c99d28e8d80edd9f374f0a09
kimy simplyi <yungookim@gmail.com>
2012-11-07 02:53:19 -0500

    added indexing feature

--- server/indexer.js
+++ server/indexer.js
@@ -1,7 +1,25 @@
 //Index given file system listing using Mongodb.
-var mongo = require('mongodb');
+var mongodb = require('mongodb'),
+    server = new mongodb.Server("127.0.0.1", 27017, {}),
+    db = new mongodb.Db('slidboard', server, {safe:false}),
+    fs = require('fs'),
+    byline = require('byline');
+ 
+module.exports.indexer = {
+	index : function() {
+		//Start reading the raw index file line by line
+		var stream = byline(fs.createReadStream('/data/test.json'));
+		db.open(function (error, client) {
+				if (error) throw error;
+					stream.on('data', function(line) {
+						var collection = new mongodb.Collection(client, 'index');
+						collection.insert(JSON.parse(line), {safe:true}, function(err, objects) {
+							if (err) console.warn(err.message);
+							if (err && err.message.indexOf('E11000 ') !== -1) {}
+      		});
+			});
+		});
+	}
+}
 
-var server = new mongo.Server("127.0.0.1", 27017, {});
-
-
-
+module.exports.indexer.index();
