697200e5eba6e04facb9dd081f81c905fec8f554
kimy simplyi <yungookim@gmail.com>
2012-11-18 19:42:55 -0500

    removing TCP completely

--- server/app.js
+++ server/app.js
@@ -5,63 +5,6 @@ var net = require('net'),
     fs = require('fs'),
     BufferedWriter = require('buffered-writer');
 
-//Using TCP
-var server = net.createServer(function (socket){
-	socket.setEncoding('utf-8');
-	socket.setNoDelay(true);
-	socket.setKeepAlive(true);
-	var remoteAddress = socket.remoteAddress;
-	var device = "";
-
-	console.log('Accepting Connection from ' + socket.remoteAddress);
-	
-	socket.on('data', function(data){
-		console.log("===================================================");
-		console.log("Raw Message : " + data);
-		//check if data is JSON
-		try {
-			var parsed_data = JSON.parse(data);
-			device = parsed_data.from;
-			console.log("Message parsed");
-			console.log(parsed_data);
-		} catch (e){
-			console.log("Wrong data time : app.js.socket.on.data");
-			console.log(e);
-			console.log(data);
-			socket.end('WDT');
-			return;
-		}
-		
-		APICalls.exec(parsed_data, function(ret){
-			if (ret === "PS.init") {
-				APICalls.pixelSense = socket;
-			} else if (ret === "Device.init") {
-				APICalls.mobiles.push(socket);
-				socket.end('ok');
-			}
-		});
-		console.log("===================================================");
-	});
-	socket.on('end', function(){
-		console.log('Connection half closed with ' + device + "(" + remoteAddress + ")");
-		socket.destroy();
-	});
-	socket.on('timeout', function(){
-		socket.end();
-		socket.destroy();
-	});
-	socket.on('error', function(e){
-		console.log(e);
-	});
-	socket.on('close', function(){
-		console.log('Connection fully closed with ' +
-				device + "(" + remoteAddress + ")");
-	});
-});
-
-server.listen(PORT, function(){
-	console.log('Server Listening on '+ PORT);
-});
 
 
 //Using HTTP
@@ -76,8 +19,7 @@ app.post('/fileIndex', function(req, res){
 	
 	var data = req.body.msg.split('\n');
 
-	//Stupid, and slow implementation.
-
+	//Stupid and slow implementation.
 	for (i in data){
 		try{
 			var temp  = JSON.parse(data[i]);
@@ -141,3 +83,65 @@ app.get('/getFile', function(req, res){
 
 app.listen(8081);
 console.log("HTTP server running on 8081");
+
+
+
+
+
+//Using TCP. DEPRECATED
+var server = net.createServer(function (socket){
+	socket.setEncoding('utf-8');
+	socket.setNoDelay(true);
+	socket.setKeepAlive(true);
+	var remoteAddress = socket.remoteAddress;
+	var device = "";
+
+	console.log('Accepting Connection from ' + socket.remoteAddress);
+	
+	socket.on('data', function(data){
+		console.log("===================================================");
+		console.log("Raw Message : " + data);
+		//check if data is JSON
+		try {
+			var parsed_data = JSON.parse(data);
+			device = parsed_data.from;
+			console.log("Message parsed");
+			console.log(parsed_data);
+		} catch (e){
+			console.log("Wrong data time : app.js.socket.on.data");
+			console.log(e);
+			console.log(data);
+			socket.end('WDT');
+			return;
+		}
+		
+		APICalls.exec(parsed_data, function(ret){
+			if (ret === "PS.init") {
+				APICalls.pixelSense = socket;
+			} else if (ret === "Device.init") {
+				APICalls.mobiles.push(socket);
+				socket.end('ok');
+			}
+		});
+		console.log("===================================================");
+	});
+	socket.on('end', function(){
+		console.log('Connection half closed with ' + device + "(" + remoteAddress + ")");
+		socket.destroy();
+	});
+	socket.on('timeout', function(){
+		socket.end();
+		socket.destroy();
+	});
+	socket.on('error', function(e){
+		console.log(e);
+	});
+	socket.on('close', function(){
+		console.log('Connection fully closed with ' +
+				device + "(" + remoteAddress + ")");
+	});
+});
+
+server.listen(PORT, function(){
+	console.log('Server Listening on '+ PORT);
+});
